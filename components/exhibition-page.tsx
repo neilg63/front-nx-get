6ac66b3c378049a1747c8a9f8523ca5df8debe58
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-08-16 17:51:23 +0100

    integrated nextui components

--- components/exhibition-page.tsx
+++ components/exhibition-page.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from "next";
 import Link from 'next/link';
 import parse from "html-react-parser";
-import { MediaItem, NodeEntity, PageDataSet } from "../lib/entity-data";
+import { MediaItem, PageDataSet } from "../lib/entity-data";
 import { BaseEntity } from "../lib/api-view-results";
+import Image from "next/image";
+import { defaultImageLoader } from "../lib/utils";
 
 const ExhibitionPage: NextPage<BaseEntity> = (data ) => {  
   const pageData = new PageDataSet(data);
@@ -12,10 +14,12 @@ const ExhibitionPage: NextPage<BaseEntity> = (data ) => {
       <h1><Link href={nextAlias}><a>{entity.title}</a></Link></h1>
       {entity.hasSubtitle && <h3 className="subtitle">{parse(entity.field_subtitle)}</h3>}
        {entity.hasBody && <div className="body">{parse(entity.body)}</div>}
-      {entity.hasImages && entity.images.map((item: MediaItem) => <figure key={item.uuid}>
-        <img srcSet={item.srcSet} src={item.medium} alt={item.alt} />
+      {entity.hasImages && <section className="media-items">
+      {entity.images.map((item: MediaItem) => <figure key={item.uri} data-key={item.uri} data-dims={item.dims('medium')}>
+        <Image loader={defaultImageLoader}  sizes={item.srcSet} src={item.medium} alt={item.alt} width={item.calcWidth('medium')} height={item.calcHeight('medium')} />
         <figcaption>{item.field_credit}</figcaption>
       </figure>)}
+    </section>}
   </article>
 }
 
