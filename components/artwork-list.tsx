48d7795092940a1a89678ade9443fcd4e1780c08
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-08-19 21:41:15 +0100

    added assets

--- components/artwork-list.tsx
+++ components/artwork-list.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
-import parse from "html-react-parser";
 import Link from 'next/link';
+import { useState, useEffect } from "react";
 import { BaseEntity } from "../lib/api-view-results";
 import { PageDataSet } from "../lib/entity-data";
 import Paginator from "./widgets/paginator";
@@ -20,6 +20,24 @@ const ArtworkList: NextPage<BaseEntity> = (data) => {
   const showPaginator = total > 0 && total > perPage;
   const hasItems = items instanceof Array && items.length > 0;
   const basePath = '/artworks/';
+  const emptyFigStyles = { width: 0, display: 'none' };
+  useEffect(() => {
+    const container = document.querySelector('.artwork-list .flex-rows-6');
+    if (container) {
+      const figs = container.querySelectorAll('figure.node');
+      const lastIndex = figs.length - 1;
+      const contRect = container.getBoundingClientRect();
+      const lastFigRect = figs[lastIndex].getBoundingClientRect();
+      const lastRowFigs = [...figs].filter(f => f.getBoundingClientRect().y === lastFigRect.y);
+      const lastRowWidth = lastRowFigs.map(f => f.getBoundingClientRect().width).reduce((a, b) => a + b, 0);
+      const remaining = contRect.width - lastRowWidth;
+      const empFig = container.querySelector('.empty-figure')
+      if (empFig instanceof HTMLElement && remaining > 10) {
+        empFig.style.display = 'block';
+        empFig.style.width = `${remaining}px`;
+      }
+    }
+  });
   return <>
     <Head>
       <SeoHead meta={meta} />
@@ -27,7 +45,7 @@ const ArtworkList: NextPage<BaseEntity> = (data) => {
     <Container {...containerProps}>
       <section className="artwork-list">
         {hasItems && <><div className="flex-rows-6">
-          {items.map(item => <figure key={item.uuid} data-key={item.uuid} data-dims={item.firstImage.dims('medium')} style={item.firstImage.calcAspectStyle()}>
+          {items.map(item => <figure key={item.uuid} data-key={item.uuid} data-dims={item.firstImage.dims('medium')} style={item.firstImage.calcAspectStyle()} className='node'>
             <Link href={item.path} className="image-holder"><a className="image-link" title={ item.numMediaLabel }>
                 {item.hasImage && <Image loader={defaultImageLoader} src={item.firstImage.preview} alt={item.alt} width={item.firstImage.calcWidth('preview')} height={item.firstImage.calcHeight('preview')} objectFit='contain' layout='intrinsic' />}
                 </a></Link>
@@ -37,7 +55,8 @@ const ArtworkList: NextPage<BaseEntity> = (data) => {
                 <TypeLink value={item.field_type} basePath={basePath} />
                 <YearLink value={item.field_year} basePath={basePath} />
               </figcaption>
-              </figure>)} 
+            </figure>)}
+          <figure className="empty-figure" style={ emptyFigStyles }></figure>
           </div>
           {showPaginator && <Paginator pageData={pageData} maxLinks={8} />}
         </>}
