afa0d2be40fdb31aeaeb5d95b702ba9d446e0206
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-08-27 16:18:27 +0100

    more work on About Page widgets and footer menu

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,7 @@
-import type { GetServerSideProps } from 'next'
 import Home from '../components/home'
 import {  fetchFullNode } from '../lib/api-view-results'
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps = async (context: any) => {
   const pageData = await fetchFullNode('home');
   return {
     props: pageData,
