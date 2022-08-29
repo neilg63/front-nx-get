5e2e666c40e800989fdfd1dc9ef08ea7c0d51684
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-08-29 11:14:26 +0100

    added continuous scroll

---
+++ lib/settings.ts
@@ -0,0 +1,20 @@
+/*
+  Miscellanous settings that are neither controlled by the CMS
+  nor environmental (.env.local), but are best grouped in one place
+*/
+
+/*
+ * Min. height at which longer continuous listings kick in
+ */
+export const minLargeSize = { width: 1024, height: 720 };
+
+export const numScrollBatches = { standard: 5, large: 10 };
+
+export const defaultDeviceSize = { width: 800, height: 600 };
+
+export const isMinLargeSize = (contextRef: any = null) => {
+  const context = contextRef instanceof Object ? contextRef : defaultDeviceSize;
+  const screenHeight = context?.height || defaultDeviceSize.height;
+  const screenWidth = context?.width || defaultDeviceSize.width;
+  return screenHeight > minLargeSize.height && screenWidth > minLargeSize.width;
+};
