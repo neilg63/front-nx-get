6d44b2ca41c4cc40d9d1a9b6d60db9e60d8bdf7e
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-08-31 12:28:24 +0100

    more work on tags layout + related artworks in exhibitions

--- components/artwork-page.tsx
+++ components/artwork-page.tsx
@@ -13,6 +13,8 @@ import Head from "next/head";
 import Carousel from "./widgets/carousel";
 import RelatedItem from "./widgets/related-item";
 import { relatedKey } from "../lib/ui-entity";
+import contentTypes from "../lib/content-types";
+import MiniRelatedItem from "./widgets/mini-related-item";
 
 const ArtworkPage: NextPage<BaseEntity> = (data) => {  
   const pageData = new PageDataSet(data);
@@ -36,9 +38,24 @@ const ArtworkPage: NextPage<BaseEntity> = (data) => {
           {entity.hasBody && <div className="body">{parse(entity.body)}</div>}
           <TagList terms={entity.field_tags} base={basePath} prefix="tag" />
         </div>
-        <div className='related-exhibitions flex-grid-2'>
-          {entity.hasRelatedExhibitions && entity.field_related_exhibitions.map((row: NodeEntity, index: number) => <RelatedItem key={relatedKey(row, index)} item={ row } />)}
-        </div>
+        {entity.hasRelatedExhibitions && <div className='related-exhibitions related'>
+          <h3>{contentTypes.exhibition}</h3>
+          <div className='flex-grid-2'>
+            {entity.field_related_exhibitions.map((row: NodeEntity, index: number) => <RelatedItem key={relatedKey(row, index)} item={row} />)}
+          </div>
+        </div>}
+        {entity.hasRelatedEssays && <div className='related-essays related'>
+          <h3>{contentTypes.article}</h3>
+          <div className='flex-grid-4'>
+            {entity.field_related_essays.map((row: NodeEntity, index: number) => <MiniRelatedItem key={relatedKey(row, index)} item={row} />)}
+          </div>
+        </div>}
+        {entity.hasRelatedPresss && <div className='related-press related'>
+          <h3>{contentTypes.press}</h3>
+          <div className='flex-grid-4'>
+            {entity.field_related_press.map((row: NodeEntity, index: number) => <MiniRelatedItem key={relatedKey(row, index)} item={row} />)}
+          </div>
+        </div>}
       </article>
     </Container>
   </>
