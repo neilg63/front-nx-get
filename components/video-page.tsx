e62f22548f2876e3839995993e71fcbfef7d5c5e
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-09-01 11:21:22 +0100

    more work on tags layout + related artworks in exhibitions

--- components/video-page.tsx
+++ components/video-page.tsx
@@ -21,19 +21,22 @@ const VideoPage: NextPage<BaseEntity> = (data ) => {
     <Head>
       <SeoHead meta={meta} />
     </Head>
-    <Container {...containerProps} className='video-container'>
+    <Container {...containerProps} className='video-container grid-sidebar'>
     <article className="video">
         <h1><Link href={nextAlias}><a>{entity.title}</a></Link></h1>
         {entity.hasVideo && <iframe className="video" src={entity.vimeoUrl} allow="allowfullscreen allowtransparency autoplay"></iframe>}
         <p>{ mediumDate(entity.field_date) }</p>
         {entity.hasBody && <div className="body">{parse(entity.body)}</div>}
+        
+      </article>
+      <aside className='sidebar sidebar-right'>
         {entity.hasRelatedVideos && <div className='related-videos related'>
           <h3>{labels.related_videos}</h3>
-          <div className='flex-grid-2'>
+          <div className='columns'>
             {entity.field_related_videos.map((row: NodeEntity, index: number) => <VideoPreview key={relatedKey(row, index)} node={row}/>)}
           </div>
         </div>}
-      </article>
+      </aside>
     </Container>
     </>
 }
