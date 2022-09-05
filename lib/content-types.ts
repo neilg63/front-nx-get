a15e5270708a8bca2201c8687b5c75986b560390
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-09-05 15:33:46 +0100

    style enhancements

--- lib/content-types.ts
+++ lib/content-types.ts
@@ -12,9 +12,28 @@ const contentTypes: StringMap = {
   publication: "Publications",
 };
 
+const contenSections: StringMap = {
+  artworks: "Artworks",
+  exhibitions: "Exhibitions",
+  news: "News",
+  videos: "GTTV",
+  articles: "Essays",
+  press: "Press releases",
+  essays: "Essays",
+};
+
+export const toBundlePlural = (bundle: string) => {
+  const keys = Object.keys(contenSections);
+  if (keys.includes(bundle)) {
+    return contenSections[bundle];
+  } else {
+    return keyToTitle(bundle);
+  }
+};
+
 export const bundleName = (bundle: string) => {
   switch (bundle) {
-    case "aricle":
+    case "article":
       return "Essay";
     case "gttv":
       return "GTTV";
