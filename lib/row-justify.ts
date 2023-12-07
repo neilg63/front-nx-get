751c07dfa47d4c1d0430fa7720eeab44f10fa9f3
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-12-07 10:29:17 +0000

    only adjusting image listing height above mobile width

--- lib/row-justify.ts
+++ lib/row-justify.ts
@@ -1,4 +1,3 @@
-import { Container } from "@nextui-org/react";
 import { singleColumnStartWidth } from "./settings";
 
 const imgPath = '.image-link';
@@ -12,89 +11,6 @@ const isAppleSafari = () => {
     return false;
   }
 }
-
-/* export const justifyRows = (containerId: string, allowAnyway = false) => {
-  const container = document.getElementById(containerId);
-  const allowOverride = allowAnyway || !isAppleSafari();
-  if (container instanceof HTMLElement && allowOverride) {
-    const containerBR = container.getBoundingClientRect();
-    const containerLeft = containerBR.left;
-    const containerWidth = containerBR.width;
-    const contRight = containerBR.right;
-    let row = 0;
-    let rowWidth = 0;
-    let pdL = 0;
-    container.querySelectorAll('figure').forEach((el, i) => {
-      if (el instanceof HTMLElement) {
-        const img = el.querySelector(imgPath);
-        if (img instanceof HTMLElement) {
-          const figBR = el.getBoundingClientRect();
-          const currLeft = figBR.left - containerLeft;
-          
-          if (i > 0 && currLeft < 10) {
-            const porWidth = containerWidth / rowWidth;
-            if (porWidth > 0.95 && porWidth < 1.8) {
-              const els = container.querySelectorAll(`.row-${row}`);
-              const numEls = els.length;
-              const lastIndex = numEls - 1;
-              if (numEls > 1) {
-                els.forEach((el, index) => {
-                  if (el instanceof HTMLElement) {
-                    if (el.classList.contains('resized') === false) {
-                      const nh = el.clientHeight * porWidth;
-                      el.style.height = `${nh}px`;
-                      el.classList.add('resized')
-                      if (index === lastIndex) {
-                        const img = el.querySelector('img');
-                        if (img instanceof HTMLElement) {
-                          const rightEndDiff = (contRight - pdL - img.getBoundingClientRect().right - (pdL / 2)) / lastIndex;
-                          for (let i = 1; i < numEls; i++) {
-                            const innerEl = els[i].querySelector(imgPath);
-                            if (innerEl instanceof HTMLElement) {
-                              innerEl.style.marginLeft = `${rightEndDiff}px`;
-                            }
-                            const fcEl = els[i].querySelector('figcaption');
-                            if (fcEl instanceof HTMLElement) {
-                              fcEl.style.marginLeft = `${rightEndDiff}px`;
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                });
-                if (els.length > 2) {
-                  if (els[0] instanceof HTMLElement && els[1] instanceof HTMLElement) {
-                    if (els[0].style.height) {
-                      const height1 = parseInt(els[0].style.height, 10);
-                      const height2 = parseInt(els[1].style.height, 10);
-                      const diff = Math.abs(height1 - height2);
-                      if (diff < 24) {
-                        els[0].style.height = els[1].style.height;
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            row++;
-            rowWidth = 0;
-          }
-          if (el.classList.contains('has-row') === false) {
-            el.classList.add(`row-${row}`, 'has-row');
-          }
-          if (pdL < 1) {
-            pdL = parseInt(window.getComputedStyle(el).paddingRight, 10);
-          }
-          rowWidth = figBR.right - containerLeft + pdL;
-        }
-      }
-    })
-    if (container.classList.contains('resizing')) {
-      container.classList.remove('resizing');
-    }
-  }
-} */
 
 export const justifyRows = (containerId: string, allowAnyway = false) => {
   const container = document.getElementById(containerId);
